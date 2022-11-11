2eafe3c608bc7d0dae7729e7dea86682ff00bf50
hoanm <mr.nmh175@gmail.com>
2022-11-11 10:01:28 +0700

    Initial project

---
+++ contracts/halo-router/src/bin/schema.rs
@@ -0,0 +1,12 @@
+use cosmwasm_schema::write_api;
+
+use halo_router::msg::{ExecuteMsg, InstantiateMsg, MigrateMsg, QueryMsg};
+
+fn main() {
+    write_api! {
+        instantiate: InstantiateMsg,
+        migrate: MigrateMsg,
+        execute: ExecuteMsg,
+        query: QueryMsg,
+    }
+}
